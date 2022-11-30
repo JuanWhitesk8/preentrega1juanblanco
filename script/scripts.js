f817807fceb80d4d3814d09e463098f58307e0fa
Juan Blanco <juanwhitesk8@gmail.com>
2022-11-29 23:21:07 -0500

    simulador mas bonito

--- script/scripts.js
+++ script/scripts.js
@@ -1,6 +1,6 @@
-alert("Bienvenido a Tropical Store Medellín, Ingrese la opcion del producto que desea comprar")
+alert("Bienvenido a 🌴Tropical Store Medellín🌴, Ingrese la opcion del producto que desea comprar")
 
-let seleccionarProductos = Number(prompt( "1-Tshirt $75 2-Croptop $50 3-Jean $120 4-Hoddie $110 0-Finalizar compra"))
+let seleccionarProductos = Number(prompt( "1-Tshirt $75 👚 \n2-Croptop $50 🩱 \n3-Jean $120 👖 \n4-Hoddie $110 🧥 \n0-Finalizar compra 💰"))
 let seleccionarCantidad
 let total = 0;
 
@@ -13,26 +13,26 @@ const cantidad = (cant, precio) => {
 while (seleccionarProductos != 0) {
   switch (seleccionarProductos) {
     case 1:
-      seleccionarCantidad= Number(prompt("el producto seleccionado es Tshirt, indique la cantidad"))
+      seleccionarCantidad= Number(prompt("el producto seleccionado es Tshirt 👚, indique la cantidad"))
             total += cantidad(seleccionarCantidad, 75)
       break
       case 2:
-        seleccionarCantidad = Number(prompt("el producto seleccionado es Croptop, indique la cantidad"))
+        seleccionarCantidad = Number(prompt("el producto seleccionado es Croptop 🩱, indique la cantidad"))
         total += cantidad(seleccionarCantidad, 50)
       break
     case 3:
-      seleccionarCantidad = Number(prompt("el producto seleccionado es Jean, indique la cantidad"))
+      seleccionarCantidad = Number(prompt("el producto seleccionado es Jean 👖, indique la cantidad"))
       total += cantidad(seleccionarCantidad, 120)
     break
     case 4:
-      seleccionarCantidad = Number(prompt("el producto seleccionado es Hoddie, indique la cantidad"))
+      seleccionarCantidad = Number(prompt("el producto seleccionado es Hoddie 🧥, indique la cantidad"))
       total += cantidad(seleccionarCantidad, 110)
     break
 
     default: 
       break
   }
-  seleccionarProductos = Number(prompt( "1-Tshirt $75 2-Croptop $50 3-Jean $120 4-Hoddie $110 0-Finalizar compra"))
+  seleccionarProductos = Number(prompt( "1-Tshirt $75 👚 \n2-Croptop $50 🩱 \n3-Jean $120 👖 \n4-Hoddie $110 🧥 \n0-Finalizar compra 💰"))
 }
 
 alert("el total de la compra es de: $" + total)
@@ -43,20 +43,20 @@ const envio = () => {
       alert("El envio es gratuito")
     }else{
       total += 70
-      alert("el costo de envio es de 70, el total es: " + total)
+      alert("el costo de envio es de $70, el total es: $" + total)
     }
 }
 
 // envio()
 
 const metodoDePago = () => {
-  let metodo = prompt("ingrese el metodo de pago, tarjeta o efectivo" ).toLowerCase()
+  let metodo = prompt("ingrese el metodo de pago, tarjeta 💳 o efectivo 💸" ).toLowerCase()
   if (metodo == "tarjeta") {
     total *= 1.1
     console.log(total)
   } else if ( metodo == "efectivo") {
     total -= 20
-    alert("tenes un descuento de 20, el total es:" + total)
+    alert("tenes un descuento de $20, el total es: $" + total)
   } else {
     alert("dato incorrecto")
   }
